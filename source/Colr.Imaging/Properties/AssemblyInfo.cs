34275f8104eeb8103cdee7e96aafd98ac476451e
smackem <philip_boger@hotmail.com>
2015-11-17 22:17:39 +0100

    Added strong name file signing

--- source/Colr.Imaging/Properties/AssemblyInfo.cs
+++ source/Colr.Imaging/Properties/AssemblyInfo.cs
@@ -7,22 +7,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Colr.Imaging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("pbo")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
-[assembly: InternalsVisibleTo("Test.Colr.Imaging")]
+[assembly: InternalsVisibleTo("Test.Colr.Imaging, PublicKey=00240000048000009400000006020000002400005253413100040000010001003d806df9974dd95e5b596b2769ddea583563ebad173b4647a01e5cc9ef6a22eab2c4e6b6b28dcff9779e0244aef2c92052086e2c4f177e70ff8911e22e28661b6957f41de63e3aec9ecd7f719df8a6ed77dd8bdf946c5731f5eb62c273c713b4fd32f5ab523ee1c7eddd9980eb24b6a063ba99ce3a491ae1f5109048f095e28a")]
---
+++ source/common/AssemblyInfo.Common.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("Smackem")]
+[assembly: AssemblyProduct("Colr.DesktopApp")]
+[assembly: AssemblyCopyright("Copyright © Smackem 2015")]
+[assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
